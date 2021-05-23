a90cffb50aa1f95c66aa77261f8c94461af97a9d
santosh.work965@gmail.com <santosh.work965@gmail.com>
2021-05-23 13:22:21 +0530

    form validity for individual input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { findByPlaceholderText } from "@testing-library/dom";
+import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouch, setEnteredNameTouch] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
 
-  const enterNameIsValid = enteredName.trim() !== '';
-  const nameInputIsInvalid = !enterNameIsValid && enteredNameTouch;
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouch;
+
+  useEffect(() => {
+    if(enteredNameIsValid){
+      setFormIsValid(true);
+    }
+    else{
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -20,7 +31,7 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouch(true);
 
-    if (!enterNameIsValid) {
+    if (!enteredNameIsValid) {
       return;
     }
     console.log(enteredName);
@@ -48,7 +59,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
